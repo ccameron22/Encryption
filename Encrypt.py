1f092fa8144cbf7460bc0b1d1fbd0de612834f94
chasecameron <chasecameron@finfolio.com>
2023-10-13 13:20:07 -0500

    Key Generator stores keys to file
    Encryption calls private key from file, not directly
    generator. Encryption works
    
    Decryption not fixed

--- Encrypt.py
+++ Encrypt.py
@@ -1,9 +1,14 @@
-from keyGenerator import publicKey
+
 from base64 import b64encode
 import rsa
 
+f = open("Private Key.pem", "r")
+privateKey = f.read()
+print(privateKey.encode('utf-8'))
+privateKey = rsa.PrivateKey.load_pkcs1(privateKey)
+
 password = input("Enter password: ")
 encodedPassword = password.encode()
-encryptedPassword = rsa.encrypt(encodedPassword, publicKey)
+encryptedPassword = rsa.encrypt(encodedPassword, privateKey)
 b64Pass = b64encode(encryptedPassword).decode()
 print(encryptedPassword)
--- keyGenerator.py
+++ keyGenerator.py
@@ -1,3 +1,14 @@
 import rsa
+from base64 import b64encode
 
 publicKey, privateKey = rsa.newkeys(512)
+print(publicKey.save_pkcs1().decode('utf-8'))
+
+f = open("Public Key.pem", "wb")
+f.write(publicKey.save_pkcs1())
+f.close()
+
+
+f = open("Private Key.pem", "w")
+f.write((privateKey.save_pkcs1().decode('utf-8')))
+f.close()
